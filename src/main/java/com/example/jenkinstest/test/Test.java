d245cefa1a01057360854e74d7b094a0b80fcabc
peng.wang <1634732309@qq.com>
2023-04-25 11:46:35 +0800

    1

--- src/main/java/com/example/jenkinstest/test/Test.java
+++ src/main/java/com/example/jenkinstest/test/Test.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
     @RequestMapping("/test")
     public String test(){
-        return "666";
+        return "111";
     }
 }
